70aa959544fe804e6b10b2af0b2e09422a936152
Sahand <sahand.prs@gmail.com>
2021-03-23 14:56:27 +0100

    Search Box and First anunce

--- app/src/main/java/com/example/stujob/MainActivity.java
+++ app/src/main/java/com/example/stujob/MainActivity.java
@@ -15,8 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }// belah belah
-    // Pride Forever
-    //jaja susanne
-    // Hola Amigo
+        setContentView(R.layout.activity_home_g_u_i);
+    }
 }
