0c9ec712face5d6e91173804bbe3f601f39258ca
Sahand <sahand.prs@gmail.com>
2021-03-23 10:50:28 +0100

    Creating Home GUI
    With this change  i (Sahand Poursadeghi) will be able to design Homewith my own Java and activity file.

---
+++ app/src/main/java/com/example/stujob/HomeGUI.java
@@ -0,0 +1,15 @@
+package com.example.stujob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeGUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_g_u_i);
+
+    }
+}
